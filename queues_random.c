9ee09effd35e4b79b93a5f96f0e0a3406ad71cc4
Shree Raksha B M <105210670+Shreeraksha9@users.noreply.github.com>
2023-06-03 01:45:34 +0530

    Add files via upload

---
+++ queues_random.c
@@ -0,0 +1,138 @@
+#include<stdio.h>
+#define max 5
+int Q[max];
+int f,r;
+FILE* fp3,*fp4,*fp5,*fp6,*fp7 ;
+int main()
+{
+     f=-1;
+     r=-1;
+    int ch;
+    int data,n,i,ele;
+    fp3=fopen("raksha.txt","w");
+     fp4=fopen("hello.txt","w");
+     fp5=fopen("positive.txt","w");
+     fp6=fopen("bcd.txt","w");
+     fp7=fopen("abc.txt","w");
+      time_t t;
+      srand((unsigned) time(&t));
+    printf("Enter the element\n");
+        scanf("%d",&n);
+        int ar2[n];
+
+    for(i=0;i<n;i++)
+    {
+        ar2[i]=rand()%1000;
+        fprintf(fp3,"%d\n",ar2[i]);
+
+    }
+   fclose(fp3);
+      fp3=fopen("raksha.txt","r");
+    while(1)
+    {
+        printf("1.Insert\n");
+        printf("2.Delete\n");
+        printf("3.Display\n");
+        printf("Exit\n");
+
+    printf("enter the choice\n");
+    scanf("%d",&ch);
+    switch(ch)
+    {
+        case 1 : if(r==max-1)
+                {
+                        printf("Queue is Full\n");
+
+                }
+                else
+                {
+                    if(n!=0)
+                       {
+                        fscanf(fp3,"%d", &data);
+                         printf("inserted element %d into the queue",data);
+                 Insert(data);
+                fprintf(fp4,"INSERTED ELEMENT %d \n",data);
+                 fprintf(fp6,"INSERT\n");
+                n--;
+
+                }
+                break;
+
+        case 2 :
+
+
+                     delete();
+                     fprintf(fp6,"DELETE\n");
+
+
+                 break;
+
+        case 3 : if(f==-1&&r==-1)
+                   {
+                       printf("Queue is empty\n");
+                   }
+                else
+                    display();
+                    break;
+        case 4 : exit(0);
+        default : printf("Invalid Choice\n");
+    }
+    }
+}
+}
+void Insert(int ele)
+{
+    if(f==-1&&r==-1)
+    {
+        f++;
+        r++;
+        Q[r]=ele;
+
+    }
+    else
+    {
+        r++;
+        Q[r]=ele;
+
+    }
+}
+
+void delete()
+{
+    if(f==-1&&r==-1)
+    {
+        printf("Queue is empty\n");
+
+    }
+    else
+    {
+        if(f==r)
+        {
+            printf("Element deleted = %d \n",Q[f]);
+            fprintf(fp4,"DELETED ELEMENT %d \n",Q[f]);
+            f=-1;
+            r=-1;
+        }
+        else
+        {
+            printf("Element deleted = %d \n",Q[f]);
+            fprintf(fp4,"DELETED ELEMENT %d \n",Q[f]);
+            f++;
+
+        }
+    }
+}
+
+void display()
+{
+    int i;
+    for(i=f;i<=r;i++)
+    {
+         printf("%d ",Q[i]);
+          fprintf(fp4,"%d\n",Q[i]);
+    }
+    printf("\n");
+     fprintf(fp4,"\n");
+
+}
+
